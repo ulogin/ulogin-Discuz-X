4fbc22445fb0a4945c9070e2f9006f7e9eb024d2
chernozor <chernozor@yandex.ru>
2015-07-28 17:08:07 +0300

    version 2.0.0

--- upload/source/plugin/ulogin/install.php
+++ upload/source/plugin/ulogin/install.php
@@ -5,8 +5,8 @@ if(!defined('IN_DISCUZ')) {
 
 $sql = <<<EOF
 CREATE TABLE IF NOT EXISTS `pre_ulogin_member` (
-  `id` mediumint(8)  NOT NULL AUTO_INCREMENT,
-  `uid` mediumint(8)  NOT NULL,
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` mediumint(8) unsigned NOT NULL,
   `identity` varchar(255)  NOT NULL,
   `network` varchar(255)  NOT NULL,
   PRIMARY KEY (`id`)
